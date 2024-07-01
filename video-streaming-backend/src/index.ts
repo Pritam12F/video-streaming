76b2a3262b1f610c91d1e03d837615c4ce32237e
PritamDas <pritam.das.santu@gmail.com>
2024-07-01 23:29:50 +0530

    started writing backend server

--- video-streaming-backend/src/index.ts
+++ video-streaming-backend/src/index.ts
@@ -1,9 +1,25 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import multer from "multer";
+import { cors } from "hono/cors";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import path from "path";
+import { exec } from "child_process";
+import { stderr, stdin } from "process";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(cors());
+app.use(async (c, next) => {
+  const contentType = c.req.header("Access-Control-Allow-Origin");
 
-export default app
+  if (contentType === "video") {
+    next();
+  }
+});
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
